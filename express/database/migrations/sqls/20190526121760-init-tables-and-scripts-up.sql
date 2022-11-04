4e4969b95440aae85d647e62103ede191b9dfcdb
Miguel Sy <miguel@saperium.com>
2022-11-04 16:07:08 +0800

    Fix Creation of Stored Procedures

--- express/database/migrations/sqls/20190526121760-init-tables-and-scripts-up.sql
+++ express/database/migrations/sqls/20190526121760-init-tables-and-scripts-up.sql
@@ -10,25 +10,26 @@ CREATE TABLE IF NOT EXISTS `tasks` (
 CREATE INDEX idx_pos ON `tasks` (`pos`);
 
 DROP PROCEDURE IF EXISTS `get_tasks`;
-CREATE PROCEDURE `get_tasks` () BEGIN
-SELECT
-  *
-FROM
-  tasks
-ORDER BY
-  pos
+CREATE PROCEDURE `get_tasks` () 
+BEGIN
+    SELECT
+        *
+    FROM
+        tasks
+    ORDER BY
+        pos;
 END;
 
 DROP PROCEDURE IF EXISTS `get_task`;
 CREATE PROCEDURE `get_task` (
     IN p_id VARCHAR(50)
 ) BEGIN
-SELECT
-  *
-FROM
-  tasks
-WHERE
-  id = p_id
+    SELECT
+        *
+    FROM
+        tasks
+    WHERE
+        id = p_id;
 END;
 
 DROP PROCEDURE IF EXISTS `create_task`;
@@ -37,24 +38,25 @@ CREATE PROCEDURE `create_task` (
     IN p_task_title VARCHAR(255),
     IN p_task_description TEXT
 ) BEGIN
-INSERT INTO tasks (
-    id,
-    task_title,
-    task_description,
-    pos
-)
-VALUES
-(
-    p_id,
-    p_task_title,
-    p_task_description,
-    (
-        SELECT
-        IF(MAX(pos) IS NULL, 0, MAX(pos) + 1)
-        FROM
-        tasks
+    INSERT INTO tasks (
+        id,
+        task_title,
+        task_description,
+        pos
     )
-);
+    VALUES
+    (
+        p_id,
+        p_task_title,
+        p_task_description,
+        (
+            SELECT
+            IF(MAX(pos) IS NULL, 0, MAX(pos) + 1)
+            FROM
+            tasks
+        )
+    );
+END;
 
 DROP PROCEDURE IF EXISTS `update_task`;
 CREATE PROCEDURE `update_task` (
@@ -62,21 +64,21 @@ CREATE PROCEDURE `update_task` (
     IN p_task_title VARCHAR(255),
     IN p_task_description TEXT
 ) BEGIN
-UPDATE 
-    tasks
-SET
-    task_title = p_task_title,
-    task_description = p_task_description
-WHERE
-    id = p_id;
+    UPDATE 
+        tasks
+    SET
+        task_title = p_task_title,
+        task_description = p_task_description
+    WHERE
+        id = p_id;
 END;
 
 DROP PROCEDURE IF EXISTS `delete_task`;
 CREATE PROCEDURE `delete_task` (
     IN p_id VARCHAR(50)
 ) BEGIN
-DELETE FROM 
-    tasks
-WHERE
-    id = p_id;
+    DELETE FROM 
+        tasks
+    WHERE
+        id = p_id;
 END;
